2723ace013658ca8b93440442b9cd3d00d71b89d
Priyadarshi <abhi.priyadarshi@live.com>
2019-05-28 08:34:03 +0530

    First time Commit

---
+++ src/test/java/JavaLearning/MavenDay1Test.java
@@ -0,0 +1,24 @@
+package JavaLearning;
+
+import org.testng.annotations.Test;
+
+public class MavenDay1Test {
+	
+		  
+	  @Test()
+	  public void test1()
+	  {
+		System.out.println("test 1");  
+	  }
+	  
+	  @Test()
+	  public void test2()
+	  {
+		System.out.println("test 2");  
+	  }
+	  
+	  @Test()
+	  public void test3()
+	  {
+		  System.out.println("Test 3");
+}}
---
+++ src/test/java/JavaLearning/SeleniumTest.java
@@ -0,0 +1,16 @@
+package JavaLearning;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void webtesting() {
+		System.out.println("testing web");
+	}
+	
+	@Test
+	public void browserTesting() {
+		System.out.println("testing Browsers");
+	}
+
+}
